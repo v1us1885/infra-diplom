45a5f51c36fd4122653a653eb75b97ae1c0fd986
FilatovAV <v1rus1885@mail.ru>
2025-03-16 21:11:07 +0000

    Fix S3 backend configuration

--- backend.tf
+++ backend.tf
@@ -4,7 +4,8 @@ terraform {
     bucket     = "bucket-tfstate"
     region     = "ru-central1"
     key        = "infra-k8s/terraform.tfstate"
-    profile    = "yandex"
+    access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
+    secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true 
