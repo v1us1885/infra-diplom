5b5b6822c2d9db97b1e42af3006154b682d675f1
FilatovAV <v1rus1885@mail.ru>
2025-03-16 21:38:36 +0000

    Fix S3 backend with force_path_style and correct working-directory

--- backend.tf
+++ backend.tf
@@ -7,7 +7,8 @@ terraform {
     profile    = "yandex"
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true 
-    skip_s3_checksum            = true 
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    force_path_style            = true
   }
 }
